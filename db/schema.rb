d95c49b7ef0c2ffbf7dbe0622e58e3489118de1f
Julio Diaz n <julio.diazn@utem.cl>
2018-11-22 23:30:25 -0300

    reparacion de commit desaparecidos

--- db/schema.rb
+++ db/schema.rb
@@ -38,16 +38,4 @@ ActiveRecord::Schema.define(version: 2018_11_13_192028) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  create_table "usuarios", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_usuarios_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true
-  end
-
 end
